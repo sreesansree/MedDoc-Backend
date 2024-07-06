2d582f2329f354d30fb3708d22e9b463568bdcfb
SREESANKAR-P <sreesankarputhumana@gmail.com>
2024-07-06 16:05:44 +0530

    database connected

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
-
+import connectDB from "./infrastructure/database/mongodb/connection.js";
 dotenv.config();
+connectDB();
+
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 5000
 
 app.use(morgan("dev"));
 app.use(cors());
+
 app.use("/", (req, res) => {
   res.send("Server Running success");
 });
