503c6a35f809edb61da4ae2d868660815c8c26a5
SREESANKAR-P <sreesankarputhumana@gmail.com>
2024-07-07 20:20:18 +0530

    added middileware for handling error

--- src/index.js
+++ src/index.js
@@ -15,11 +15,22 @@ app.use(morgan("dev"));
 app.use(cors());
 
 app.use(express.json());
+
 app.use("/api/users", userRoutes);
 
 app.use("/", (req, res) => {
   res.status(200).send({
     message: "Server Running",
+  });
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
   });
 });
 
