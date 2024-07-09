31c8c874bcb73a379cb26cb5ece1732956e21f57
SREESANKAR-P <sreesankarputhumana@gmail.com>
2024-07-10 00:20:41 +0530

    Added package Cookie-parser

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import connectDB from "./infrastructure/database/mongodb/connection.js";
 import userRoutes from "./infrastructure/api/routes/userRoutes.js";
 
@@ -25,6 +26,7 @@ app.use(cors(corsOptions));
 // app.use(cors());
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 
